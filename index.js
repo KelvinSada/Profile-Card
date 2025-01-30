cb2f850b63e30034b0c4299276e0b86b91efa11a
Novoh .K. Sada <105117246+KelvinSada@users.noreply.github.com>
2025-01-30 11:17:52 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ const currentTime = document.getElementById("current-time");
 
 function locateTime(){
 
-  const seconds = new Date().getUTCSeconds()
-  const minutes = new Date().getUTCMinutes()
+  const second = new Date().getUTCSeconds()
+  const minute = new Date().getUTCMinutes()
+  
+  const minutes = minute<10?`0`+minute:minute;
+  const seconds = second<10?`0`+second:second;
+  
   const hours = new Date().getUTCHours()
 
   const utcTime = `Time: ${hours}:${minutes}:${seconds}`
